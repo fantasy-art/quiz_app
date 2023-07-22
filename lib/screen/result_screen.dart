9cd8768b2a5558dbe5da525acf5826b002d2c360
يحيى حمود <fantasy-art@live.com>
2023-07-23 01:33:54 +0300

    add result map

--- lib/screen/result_screen.dart
+++ lib/screen/result_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/question.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({super.key, required this.choosenAnswer});
+  final List<String> choosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choosenAnswer[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
